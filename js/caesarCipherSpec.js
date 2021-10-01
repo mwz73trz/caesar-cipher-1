fa0ecc87f73bfe45aa9ad6570f0871b4d0afcb51
mwz73trz <michaelzura@bellsouth.net>
2021-10-01 07:49:32 -0400

    py/js challenge completed

--- js/caesarCipherSpec.js
+++ js/caesarCipherSpec.js
@@ -1,7 +1,29 @@
 // Rewrite this in Unit Test
 
-var cs = require("./caesarCipher");
+// var cs = require("./caesarCipher");
 
-console.log(cs.caesarCipher("Boy! What a string!", -5) === "Wjt! Rcvo v nomdib!")
-console.log(cs.caesarCipher("Hello zach168! Yes here.", 5) === "Mjqqt efhm168! Djx mjwj.")
-console.log(cs.caesarCipher("Hello Zach168! Yes here.", -5) === "Czggj Uvxc168! Tzn czmz.")
+// console.log(cs.caesarCipher("Boy! What a string!", -5) === "Wjt! Rcvo v nomdib!")
+// console.log(cs.caesarCipher("Hello zach168! Yes here.", 5) === "Mjqqt efhm168! Djx mjwj.")
+// console.log(cs.caesarCipher("Hello Zach168! Yes here.", -5) === "Czggj Uvxc168! Tzn czmz.")
+
+const { expect } = require("@jest/globals");
+const cs = require("./caesarCipher");
+
+test("Is it a string?", () => {
+  expect(typeof cs.caesarCipher("test", 4)).toBe("string");
+});
+
+test("Does it handle various character inputs properly?", () => {
+  // Boy what a string test
+  expect(cs.caesarCipher("Boy! What a string!", -5)).toBe(
+    "Wjt! Rcvo v nomdib!"
+  );
+
+  //Test for numeral/punctuation handling
+  expect(cs.caesarCipher("Hello zach168! Yes here.", 5)).toBe(
+    "Mjqqt efhm168! Djx mjwj."
+  );
+  expect(cs.caesarCipher("Hello Zach168! Yes here.", -5)).toBe(
+    "Czggj Uvxc168! Tzn czmz."
+  );
+});
--- python/caesar_cipher_spec.py
+++ python/caesar_cipher_spec.py
@@ -2,6 +2,22 @@
 
 from caesar_cipher import caesar_cipher
 
-print(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
-print(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
-print(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
+# print(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
+# print(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
+# print(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
+
+import unittest
+
+class TestCaeserCipher(unittest.TestCase) :
+
+    def test_caeser_cipher_0(self) :
+        self.assertEqual(caesar_cipher("Boy! What a string!", -5), "Wjt! Rcvo v nomdib!")
+
+    def test_caeser_cipher_1(self) :
+        self.assertEqual(caesar_cipher("Hello zach168! Yes here.", 5), "Mjqqt efhm168! Djx mjwj.")
+
+    def test_caeser_cipher_2(self) :
+        self.assertEqual(caesar_cipher("Hello Zach168! Yes here.", -5), "Czggj Uvxc168! Tzn czmz.")
+
+if __name__ == '__main__':
+    unittest.main()
